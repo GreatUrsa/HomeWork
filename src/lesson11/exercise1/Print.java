718d5ad8bb79a2d1a20e7d0ee514d98e7edc20c6
GreatUrsa <thegreat.bear@mail.ru>
2017-10-30 19:27:07 +0200

    to fix

--- src/lesson11/exercise1/Print.java
+++ src/lesson11/exercise1/Print.java
@@ -2,28 +2,28 @@ package lesson11.exercise1;
 
 public class Print {
     public static void main(String[] args) {
-        Printable[] printable = new Printable[3];
-        printable[0] = new Book("Jane Eyre");
-        printable[1] = new Magazine("Rolling Stone");
-        printable[2] = new Book("Pride and Prejudice");
-        for (Printable a : printable) {
-            a.print();
+        Printable[] printables = new Printable[3];
+        printables[0] = new Book("Jane Eyre");
+        printables[1] = new Magazine("Rolling Stone");
+        printables[2] = new Book("Pride and Prejudice");
+        for (Printable printable : printables) {
+            printable.print();
         }
-        printBooks(printable);
-        printMagazines(printable);
+        printBooks(printables);
+        printMagazines(printables);
     }
 
     public static void printMagazines(Printable[] printable) {
-        for (Printable magazines : printable) {
-            if (magazines instanceof Magazine)
-                System.out.println("Журнал " + ((Magazine) magazines).getName());
+        for (Printable magazine : printable) {
+            if (magazine instanceof Magazine)
+                System.out.println("Журнал " + ((Magazine) magazine).getName());
         }
     }
 
     public static void printBooks(Printable[] printable) {
-        for (Printable books : printable) {
-            if (books instanceof Book) {
-                System.out.println("Книга " + ((Book) books).getName());
+        for (Printable book : printable) {
+            if (book instanceof Book) {
+                System.out.println("Книга " + ((Book) book).getName());
             }
         }
     }
