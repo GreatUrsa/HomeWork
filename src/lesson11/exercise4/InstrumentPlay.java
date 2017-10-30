dbcbe6e4e369c19e8f87d4030b87873b5f17b08e
GreatUrsa <thegreat.bear@mail.ru>
2017-10-30 20:00:40 +0200

    to fix

--- src/lesson11/exercise4/InstrumentPlay.java
+++ src/lesson11/exercise4/InstrumentPlay.java
@@ -2,13 +2,13 @@ package lesson11.exercise4;
 
 public class InstrumentPlay {
     public static void main(String[] args) {
-        Instrument[] instrument = new Instrument[3];
-        instrument[0] = new Drum("большой эстрадный: 580х350 мм");
-        instrument[1] = new Trumpet(123);
-        instrument[2] = new Guitar(4);
-        for (Instrument instruments : instrument) {
-            instruments.play();
-            System.out.println(instruments);
+        Instrument[] instruments = new Instrument[3];
+        instruments[0] = new Drum("большой эстрадный: 580х350 мм");
+        instruments[1] = new Trumpet(123);
+        instruments[2] = new Guitar(4);
+        for (Instrument instrument : instruments) {
+            instrument.play();
+            System.out.println(instrument);
         }
     }
 }
