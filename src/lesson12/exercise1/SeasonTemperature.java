e3dd8f0033546552467851dca0033c62f2bf457d
GreatUrsa <thegreat.bear@mail.ru>
2017-11-05 18:47:23 +0200

    Добавила метод getDescription.

--- src/lesson12/exercise1/SeasonTemperature.java
+++ src/lesson12/exercise1/SeasonTemperature.java
@@ -3,9 +3,16 @@ package lesson12.exercise1;
 /**
  * Перечисление должно содержать переменную, содержащую среднюю температуру в каждом времени года.
  * Добавить конструктор принимающий на вход среднюю температуру.
+ * Создать метод getDescription, возвращающий строку “Холодное время года”.
+ * Переопределить метод getDescription - для константы Лето метод должен возвращать “Теплое время года”.
+ * В цикле распечатать все времена года, среднюю температуру и описание времени года.
  */
 public enum SeasonTemperature {
-    WINTER(-10), SPRING(11), SUMMER(26), AUTUMN(6);
+    WINTER(-10), SPRING(11), SUMMER(26) {
+        public String getDescription() {
+            return "Тёплое время года";
+        }
+    }, AUTUMN(6);
 
     SeasonTemperature(int degrees) {
         this.degrees = degrees;
@@ -15,5 +22,9 @@ public enum SeasonTemperature {
 
     public int getDegrees() {
         return degrees;
+    }
+
+    public String getDescription() {
+        return "Холодное время года";
     }
 }
