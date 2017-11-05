5f66e4f28c8868084f8cf94c096333d2ec4b0830
GreatUrsa <thegreat.bear@mail.ru>
2017-11-05 22:43:04 +0200

    to fix

---
+++ src/lesson13/LikeJava.java
@@ -0,0 +1,25 @@
+package lesson13;
+
+/**
+ * a) Напишите метод, который принимает в качестве параметра любую строку, например “I like Java!!!”.
+ * б) Распечатать последний символ строки. Используем метод String.charAt().
+ * в) Проверить, заканчивается ли ваша строка подстрокой “!!!”. Используем метод String.endsWith().
+ * г) Проверить, начинается ли ваша строка подстрокой “I like”. Используем метод String.startsWith().
+ * д) Найти позицию подстроки “Java” в строке “I like Java!!!”.
+ */
+public class LikeJava {
+    public static void main(String[] args) {
+        String str1 = new String();
+        str1 = "I like Java!!!";
+
+        LikeJava.check(str1);
+    }
+
+    public static void check(String str) {
+        System.out.println(str);
+        System.out.println(str.charAt(str.length() - 1));
+        System.out.println(str.endsWith("!!!"));
+        System.out.println(str.startsWith("I like"));
+        System.out.println("indexOf(Java) = " + str.indexOf("Java"));
+    }
+}
