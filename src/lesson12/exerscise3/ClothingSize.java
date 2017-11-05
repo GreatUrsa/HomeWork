e3f9c8bd3fd68f02dfa0db7190ec5d6363fcee43
GreatUrsa <thegreat.bear@mail.ru>
2017-11-05 18:58:25 +0200

    Добавила метод getDescription.

--- src/lesson12/exerscise3/ClothingSize.java
+++ src/lesson12/exerscise3/ClothingSize.java
@@ -8,11 +8,19 @@ package lesson12.exerscise3;
  * Создать конструктор, принимающий на вход euroSize.
  */
 public enum ClothingSize {
-    XXS(32), XS(34), S(36), M(38), L(40);
+    XXS(32) {
+        public String getDescription() {
+            return "Детский размер";
+        }
+    }, XS(34), S(36), M(38), L(40);
 
     private int euroSize;
 
     ClothingSize(int euroSize) {
         this.euroSize = euroSize;
+    }
+
+    public String getDescription() {
+        return "Взрослый размер";
     }
 }
