e03e4556d28071aad6ac58837eaebbe09461aa88
GreatUrsa <thegreat.bear@mail.ru>
2017-11-06 21:21:12 +0200

    Задание с урока.

---
+++ src/lesson13/Person.java
@@ -0,0 +1,60 @@
+package lesson13;
+
+/**
+ * Переделать toString() для класса Person используя форматирование. 
+ */
+public class Person {
+    private String fullName;
+    private int age;
+
+    public Person(String fullName, int age) {
+        this.fullName = fullName;
+        this.age = age;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (age != person.age) return false;
+        return fullName != null ? fullName.equals(person.fullName) : person.fullName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = fullName != null ? fullName.hashCode() : 0;
+        result = 31 * result + age;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "fullName='" + fullName + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
