1b6a98c3652823c11b2d4d38ca29c2f6fdd9aa22
GreatUrsa <thegreat.bear@mail.ru>
2017-11-06 15:28:37 +0200

    to fix

--- src/lesson11/exercise3/p1/Example2.java
+++ src/lesson11/exercise3/p1/Example2.java
@@ -3,18 +3,18 @@ package lesson11.exercise3.p1;
 public class Example2 extends Example1 {
     public static void main(String[] args) {
         System.out.println("Подкласс, находящийся в том же пакете, что и суперкласс:");
-        Example1 example1 = new Example1();
+        Example2 example2 = new Example2();
 
         System.out.println("а) переменные:");
-//        System.out.println(example1.a);
-        System.out.println(example1.b);
-        System.out.println(example1.c);
-        System.out.println(example1.d);
+//        System.out.println(example2.a);
+        System.out.println(example2.b);
+        System.out.println(example2.c);
+        System.out.println(example2.d);
 
         System.out.println("б) методы:");
-//        example1.displayA();
-        example1.displayB();
-        example1.displayC();
-        example1.displayD();
+//        example2.displayA();
+        example2.displayB();
+        example2.displayC();
+        example2.displayD();
     }
 }
--- src/lesson11/exercise3/p2/Example4.java
+++ src/lesson11/exercise3/p2/Example4.java
@@ -5,18 +5,18 @@ import lesson11.exercise3.p1.Example1;
 public class Example4 extends Example1 {
     public static void main(String[] args) {
         System.out.println("Подкласс, находящийся в другом пакете:");
-        Example1 example1 = new Example1();
+        Example4 example4 = new Example4();
 
         System.out.println("а) переменные:");
-//        System.out.println(example1.a);
-//        System.out.println(example1.b);
-//        System.out.println(example1.c);
-        System.out.println(example1.d);
+//        System.out.println(example4.a);
+//        System.out.println(example4.b);
+        System.out.println(example4.c);
+        System.out.println(example4.d);
 
         System.out.println("б) методы:");
-//        example1.displayA();
-//        example1.displayB();
-//        example1.displayC();
-        example1.displayD();
+//        example4.displayA();
+//        example4.displayB();
+        example4.displayC();
+        example4.displayD();
     }
 }
