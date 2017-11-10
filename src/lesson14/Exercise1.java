cd3dfe812c3054d4a5f30c63bf8677eb0a3fd079
GreatUrsa <thegreat.bear@mail.ru>
2017-11-10 13:19:46 +0200

    to fix

--- src/lesson14/Exercise1.java
+++ src/lesson14/Exercise1.java
@@ -8,8 +8,8 @@ import java.util.regex.Pattern;
  */
 public class Exercise1 {
     public static void main(String[] args) {
-        // String regex = "(\\w{4} +\\s|\\d)";
-        String regex = "(Java\\s*\\d)";
+        String regex = "(\\w{4} +\\s|\\d)";
+        //String regex = "(Java\\s*\\d)";
         String s = "Versions: Java  5, Java 6, Java   7, Java 8.";
         Pattern pattern = Pattern.compile(regex);
         Matcher matcher = pattern.matcher(s);
