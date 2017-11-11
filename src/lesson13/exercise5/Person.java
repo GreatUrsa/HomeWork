04d34d26cd29ec564e79514975d49ad010636b28
GreatUrsa <thegreat.bear@mail.ru>
2017-11-11 14:29:54 +0200

    Добавила exercise5 (Person).

---
+++ src/lesson13/exercise5/Person.java
@@ -0,0 +1,51 @@
+package lesson13.exercise5;
+
+public class Person {
+    private String fullName;
+    private int age;
+
+    public Person(String fullName, int age) {
+        this.fullName = fullName;
+        this.age = age;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Person person = (Person) o;
+
+        if (age != person.age) return false;
+        return fullName != null ? fullName.equals(person.fullName) : person.fullName == null;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = fullName != null ? fullName.hashCode() : 0;
+        result = 31 * result + age;
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Person %cfullName=%s, age=%d%c", '{', fullName, age, '}');
+    }
+}
+
---
+++ src/lesson13/exercise5/PersonDemo.java
@@ -0,0 +1,8 @@
+package lesson13.exercise5;
+
+public class PersonDemo {
+    public static void main(String[] args) {
+        Person person = new Person("Kate", 17);
+        System.out.println(person);
+    }
+}
