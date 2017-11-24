f78e13354d3c17395c32718dd458cbd014192de9
GreatUrsa <thegreat.bear@mail.ru>
2017-11-24 17:16:42 +0200

    to fix

--- src/lesson12/DoubleDemo.java
+++ src/lesson12/DoubleDemo.java
@@ -2,13 +2,13 @@ package lesson12;
 
 public class DoubleDemo {
     public static void main(String[] args) {
-        Double aDouble1=2.3;
-        Double aDouble2=new Double("d2");
+        Double aDouble1 = 2.3;
+        Double aDouble2 = new Double("d2");
 
         System.out.println(aDouble1);
         System.out.println(aDouble2);
 
-        Double d1=Double.valueOf(3.14);
+        Double d1 = Double.valueOf(3.14);
 
         System.out.println(aDouble1.byteValue());
         System.out.println(aDouble1.shortValue());
@@ -16,7 +16,6 @@ public class DoubleDemo {
         System.out.println(aDouble1.longValue());
         System.out.println(aDouble1.floatValue());
 
-        String d=Double.toString(2.78);
-
+        String d = Double.toString(2.78);
     }
 }
