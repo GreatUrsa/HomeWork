0b49a5b117c1cb37b1e05b632cf5d076710c41ae
GreatUrsa <thegreat.bear@mail.ru>
2017-11-27 17:28:02 +0200

    to fix

--- src/lesson17/exercise4/ProductDemo.java
+++ src/lesson17/exercise4/ProductDemo.java
@@ -1,7 +1,6 @@
 package lesson17.exercise4;
 
-import java.util.SortedSet;
-import java.util.TreeSet;
+import java.util.*;
 
 /**
  * Создать несколько объектов класса Категория.
@@ -10,19 +9,57 @@ import java.util.TreeSet;
  */
 public class ProductDemo {
     public static void main(String[] args) {
-        ProductComparatorName productComparatorName = new ProductComparatorName();
-        SortedSet<Product> productsName = new TreeSet<>(productComparatorName);
-        ProductComparatorPrice productComparatorPrice = new ProductComparatorPrice();
-        SortedSet<Product> productsPrice = new TreeSet<>(productComparatorPrice);
-        ProductComparatorRating productComparatorRating = new ProductComparatorRating();
-        SortedSet<Product> productsRating = new TreeSet<>(productComparatorRating);
+        Category flowers = new Category("Flowers", new HashSet<>(Arrays.asList(
+                new Product("Peony", 30, 2),
+                new Product("Tulip", 25, 4),
+                new Product("Lotus", 40, 3),
+                new Product("Carnation", 15, 5),
+                new Product("Rose", 35, 1)
+        )));
+        Category clothes = new Category("Clothes", new HashSet<>(Arrays.asList(
+                new Product("Tie", 270, 2),
+                new Product("TShirt", 450, 5),
+                new Product("Skirt", 875, 1),
+                new Product("Trousers", 690, 3),
+                new Product("Dress", 950, 4)
+        )));
+        Category drinks = new Category("Drinks", new HashSet<>(Arrays.asList(
+                new Product("soda water", 10, 4),
+                new Product("still water", 8, 3),
+                new Product("coffee", 10, 5),
+                new Product("lemonade", 10, 2),
+                new Product("tea", 10, 1)
+        )));
 
-        productsName.add(new Product("Peony", 30, 2));
-        productsName.add(new Product("Tulip", 25, 4));
-        productsName.add(new Product("Lotus", 40, 3));
-        productsName.add(new Product("Carnation", 15, 5));
-        productsName.add(new Product("Rose", 35, 1));
+        printByName(flowers, clothes, drinks);
+        printByPrice(flowers, clothes, drinks);
+        printByRating(flowers, clothes, drinks);
+    }
 
-        Category flowers = new Category("Flowers", productsName);
+    public static void printProducts(Set<Product> sorted, Category... categories) {
+        for (Category c : categories) {
+            sorted.addAll(c.getProducts());
+        }
+        for (Product product : sorted) {
+            System.out.println(product);
+        }
+    }
+
+    public static void printByName(Category... categories) {
+        SortedSet<Product> byName = new TreeSet<>(new ProductComparatorName());
+        System.out.println("==========sorted by name==========");
+        printProducts(byName, categories);
+    }
+
+    public static void printByPrice(Category... categories) {
+        SortedSet<Product> byPrice = new TreeSet<>(new ProductComparatorPrice());
+        System.out.println("==========sorted by price==========");
+        printProducts(byPrice, categories);
+    }
+
+    public static void printByRating(Category... categories) {
+        SortedSet<Product> byRating = new TreeSet<>(new ProductComparatorRating());
+        System.out.println("==========sorted by rating==========");
+        printProducts(byRating, categories);
     }
 }
