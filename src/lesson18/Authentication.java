5cb93b67877acc83708025fb0b583a3d0cde16c4
GreatUrsa <thegreat.bear@mail.ru>
2017-11-27 11:08:51 +0200

    Задание с урока

---
+++ src/lesson18/Authentication.java
@@ -0,0 +1,30 @@
+package lesson18;
+
+public class Authentication {
+    public static void main(String[] args) {
+        try {
+            System.out.println(isCorrect("login1", "password1", "password1"));
+            System.out.println(isCorrect("login1login1login1login1login1login1login1",
+                    "password1", "password1"));
+            System.out.println(isCorrect("login1", "password1password1password1", "password1password1password1"));
+            System.out.println(isCorrect("login1", "password1", "password2"));
+            System.out.println(isCorrect("login1login1login1login1", "password1password1password1", "password2"));
+        } catch (WrongLoginException e) {
+            e.printStackTrace();
+        } catch (WrongPasswordException e) {
+            e.printStackTrace();
+        }
+        System.out.println("Конец!!!");
+    }
+
+    public static boolean isCorrect(String login, String password, String confPassword)
+            throws WrongLoginException, WrongPasswordException {
+        if (login.length() > 0) {
+            throw new WrongLoginException("Неправильный логин");
+        }
+        if (password.length() > 20 || !password.equals(confPassword)) {
+            throw new WrongPasswordException("Неправильный пароль");
+        }
+        return true;
+    }
+}
