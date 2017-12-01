a6b51a756faa3ba6022e3dc6624af72bc9a5f8af
GreatUrsa <thegreat.bear@mail.ru>
2017-12-01 21:46:00 +0200

    Задание с урока

--- src/lesson21/NewThread.java
+++ src/lesson21/NewThread.java
@@ -4,7 +4,7 @@ public class NewThread extends Thread {
     @Override
     public void run() {
         for (int i = 0; i < 100; i++) {
-            System.out.println("M");
+            System.out.println("M " +Thread.currentThread().getName());
         }
     }
 }
